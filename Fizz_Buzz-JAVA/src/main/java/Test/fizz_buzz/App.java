9ba9455b5b6fe7466ba84989dee5438283c8b454
Ships <macbookpro@Ships-mac.local>
2019-04-12 15:18:44 +0200

    FizzBuzz in the console

---
+++ Fizz_Buzz-JAVA/src/main/java/Test/fizz_buzz/App.java
@@ -0,0 +1,14 @@
+package Test.fizz_buzz;
+
+
+
+public class App 
+{
+    public static void main( String[] args ) throws InterruptedException
+    {
+    	Fizz_Buzz fzbz = new Fizz_Buzz();
+    	
+    	fzbz.fizzBuzz();
+    	
+    }
+}
